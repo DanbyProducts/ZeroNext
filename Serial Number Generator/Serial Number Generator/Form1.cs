b123261cd78b597d34ff5bf25d5ee6a62179574b
DANBY\ssapra <ssapra@danby.com>
2017-02-08 17:21:32 -0500

    Added a comment.

--- Serial Number Generator/Serial Number Generator/Form1.cs
+++ Serial Number Generator/Serial Number Generator/Form1.cs
@@ -12,6 +12,29 @@ namespace Serial_Number_Generator
 {
     public partial class Form1 : Form
     {
+
+        private string[,] ProductCodes = new string[4,2] 
+        {
+            {"30","Refrigerator - Compact, under 7.75 cu ft"},
+            {"31","Refrigerator - Mid-Size, 7.75 - 12.3 cu ft"},
+            {"32","Refrigerator - Large, greater than 12.3 cu ft"},
+            {"33","Freezer - Chest"},
+
+        };
+
+
+
+        //Format is  : APPRISE CODES , NAME , LOCATION, FACTORY I.D
+        private string[,] FactoryID = new string[4, 4]
+        {
+            {"GALANZ2","Galanz New Plant","Zhongshan","40"},
+            {"HEFEIHL","Hefei Hualing,Jinxiu Ave","Hefei,Juixiu Ave","41"},
+            {"XINGXI","XingXing","Taizhou","42"},
+            {"XINGXIFOS2","XingXing","Foshan","43"}
+        };
+
+        
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +42,7 @@ namespace Serial_Number_Generator
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //okay
         }
     }
 }
